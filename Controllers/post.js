e431c7af6e64f9c07c420fda6eba83a96804df33
Savithasamraj <103304359+Savithasamraj@users.noreply.github.com>
2022-12-21 14:58:06 +0530

    Update post.js

--- Controllers/post.js
+++ Controllers/post.js
@@ -4,7 +4,7 @@ const  datas= async(req,res)=>{
     
     try{
  const  jobdetails= await data.create(req.body)
- res.status(200).send(req.body
+ res.status(200).send(message:"profile created successfully"
     )
     }
     catch(error){

3817cea37a1ee34fb7688d3499d4065915187f22
Savithasamraj <103304359+Savithasamraj@users.noreply.github.com>
2022-12-22 15:20:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const routes=require("./Routes/jobdetails")
 mongoose.connect("mongodb+srv://savitha:Savitha19@cluster0.ngd5ggy.mongodb.net/preflight?retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology:true}).
 then(()=>console.log("connection successfull"))
